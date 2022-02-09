83292c88e674fc290cc6ea5df027afc31bdb10c6
Inimesh <ijmeshoulam@gmail.com>
2022-02-09 17:32:43 +0000

    Fixing cors whitlist to localhost:3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000" # Change this for the actual url in production
+    origins "http://localhost:3001" # Change this for the actual url in production
 
     resource "*",
       headers: :any,
