ba721e2655d4db612de763a38824b0f5d6153ff3
Sarah <dousse.sarah@gmail.com>
2022-02-09 22:57:40 +0000

    create own authenticate_user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
-  + validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
   
   def generate_jwt
     JWT.encode({ id: id,
