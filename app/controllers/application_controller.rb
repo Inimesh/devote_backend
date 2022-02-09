25f9fc91a78c35658c845809733c3144a335abee
Sarah <dousse.sarah@gmail.com>
2022-02-09 23:00:19 +0000

    override devise's authenticate_user! with the one previously created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,24 @@ class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session
 
   respond_to :json
-  
+
   before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   private
+  
+  def authenticate_user!(options = {})
+    head :unauthorized unless signed_in?
+  end
+
+  def current_user
+    @current_user ||= super || User.find(@current_user_id)
+  end
+
+  def signed_in?
+    @current_user_id.present?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
