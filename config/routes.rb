aadee2d40f7ad924c03158c5a6673fda6f97cca6
Francesco <francescocollotto@gmail.com>
2022-02-10 12:17:44 +0000

    add transactions table with amount and round_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     # TODO Resource routes go in here
     resources :configs
+    resources :transactions
   end
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_09_162612) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_115420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_09_162612) do
     t.integer "round_up_to"
     t.boolean "percentage"
     t.integer "round_up_cap"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.decimal "amount"
+    t.decimal "round_up"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
