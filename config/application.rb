6c26af81f29b4084f5557519f0a4dd97160d0e4c
Sarah <dousse.sarah@gmail.com>
2022-02-10 14:09:25 +0000

    fix glitch and generate user model

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module DevoteBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
 
-    config.middleware.use ActionDispatch::Cookies    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     config.api_only = false
   end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,13 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins "http://localhost:3001" # Change this for the actual url in production
-
-    resource "*",
+    origins 'http://localhost:3000'
+  
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
