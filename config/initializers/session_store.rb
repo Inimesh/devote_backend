cc4b10463deb1f966c50643944bd45761a62de85
Sarah <dousse.sarah@gmail.com>
2022-02-10 13:59:27 +0000

    configuring rails app for cookies

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env === 'production' 
+    Rails.application.config.session_store :cookie_store, key: '_devote_frontend', domain: '_devote_backend'
+else
+    Rails.application.config.session_store :cookie_store, key: '_devote_frontend'
+end
